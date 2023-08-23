5e0124d691f1ff16b26bb672136cfcee27ade197
Pritam Gain <pritamjob2021@gmail.com>
2023-08-23 23:21:48 +0530

    d

--- app/wanted/page.jsx
+++ app/wanted/page.jsx
@@ -7,7 +7,7 @@ import "./wanted.css";
 
 const Wanted = () => {
   return (
-    <div className=" flex-col w-max inline-block align-middle  bg-red-500 justify-center">
+    <div className=" flex-col inline-block align-middle w-auto bg-red-500 justify-center">
 
         {/* paper heading */}
 
